766b7b75abf8ee261053e97dfd38ba0c44464dae
Pratham Patel <97801784+PrathamPatel010@users.noreply.github.com>
2023-09-01 18:46:41 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,39 +12,38 @@ const manifestForPlugin = {
                 src: "/android-chrome-192x192.png",
                 sizes: "192x192",
                 type: "image/png",
-                purpose: "any maskable"
+                purpose: "maskable any"
             },
             {
                 src: "/android-chrome-512x512.png",
                 sizes: "512x512",
                 type: "image/png",
-                purpose: "any maskable"
+                purpose: "maskable any"
             },
             {
                 src: "/apple-touch-icon.png",
                 sizes: "180x180",
                 type: "image/png",
-                purpose: "any maskable"
+                purpose: "maskable any"
             },
             {
                 src: "/quote.png",
                 sizes: "256x256",
                 type: "image/png",
-                purpose: "any maskable"
+                purpose: "maskable any"
             },
         ],
         theme_color: "#171717",
         background_color: "#e8ebf2",
         display: "standalone",
         scope: "/",
-        start_url: "/",
+        start_url: "./index.html",
         orientation: "portrait",
     },
 };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
     plugins: [react(), VitePWA(manifestForPlugin)],
     build: {
         commonjsOptions: {
